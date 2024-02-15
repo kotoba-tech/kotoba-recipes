ebb74c40c7e460167e25ecfe735f6a76228c03b4
kazuki <kaz.tokyo.tech20@gmail.com>
2024-02-15 09:23:50 +0900

    chore: update convert script

--- tools/checkpoint-convert/scripts/convert.sh
+++ tools/checkpoint-convert/scripts/convert.sh
@@ -1,53 +1,31 @@
 #!/bin/bash
-#$ -l rt_AF=1
-#$ -l h_rt=10:00:00
-#$ -j y
-#$ -o outputs/convert/
-#$ -cwd
-# module load
-source /etc/profile.d/modules.sh
-module load cuda/11.8/11.8.0
-module load cudnn/8.9/8.9.2
-module load nccl/2.16/2.16.2-1
-module load hpcx/2.12
 
 set -e
 
 # swich virtual env
 source .env/bin/activate
 
-# distributed settings
-export MASTER_ADDR=$(/usr/sbin/ip a show | grep inet | grep 192.168.205 | head -1 | cut -d " " -f 6 | cut -d "/" -f 1)
-export MASTER_PORT=$((10000 + ($SLURM_JOBID % 50000)))
-
-echo "MASTER_ADDR=${MASTER_ADDR}"
-
 # fsdp
-start=600
-end=800
-increment=200
+start=2234
+end=2234
+increment=5000
 
 for ((i = start; i <= end; i += increment)); do
   ITERATION=$i
   FORMATTED_ITERATION=$(printf "iter_%07d" $ITERATION)
 
-  CHECK_POINT_PATH=/path/to/llama/checkpoints/llama-2-7b-base/instruction/4%-sq-512/${FORMATTED_ITERATION}/model.pt
-  OUTPUT_PATH=/path/to/llama/checkpoints/parallel/4%/instruction/initial/normal/hf_checkpoint
+  CHECK_POINT_PATH=/model/fujii/checkpoints/Mistral-7b-VE-algebric-stack/ichikara-lr_1e-5-minlr_1e-6/${FORMATTED_ITERATION}/model.pt
+  OUTPUT_PATH=/model/fujii/hf_checkpoints/mistral-7b-ve-algebric-stack/ichikara/${FORMATTED_ITERATION}
 
   echo "convert ${CHECK_POINT_PATH} to ${OUTPUT_PATH}"
 
   mkdir -p $OUTPUT_PATH
 
-  BASE_MODEL_CHECKPOINT=/path/to/llama2/Llama-2-7b-hf
+  BASE_MODEL_CHECKPOINT=/model/fujii/hf_checkpoints/Mistral-7B-VE-algebraic-stack-lr_2e-5-minlr_6.6e-7_warmup_1000-iter0025000/
 
-  mpirun -np 8 \
-    --npernode 8 \
-    -x MASTER_ADDR=$MASTER_ADDR \
-    -x MASTER_PORT=$MASTER_PORT \
-    -bind-to none -map-by slot \
-    -x PATH \
-    python tools/convert_ckpt.py \
+  python tools/checkpoint-convert/convert_ckpt.py \
     --model $BASE_MODEL_CHECKPOINT \
     --ckpt $CHECK_POINT_PATH \
-    --out $OUTPUT_PATH
+    --out $OUTPUT_PATH \
+    --sequence-length 4096
 done
