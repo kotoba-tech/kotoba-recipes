da2df7ac47282848a55e261fd62964f838bc2fd5
kazuki <kaz.tokyo.tech20@gmail.com>
2024-02-18 00:59:04 +0900

    Update model paths and upload script

--- tools/model-upload/upload.sh
+++ tools/model-upload/upload.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-start=2234
-end=2234
+start=672
+end=672
 increment=5000
 
-upload_base_dir=/model/fujii/hf_checkpoints/swallow-13b/ichikara
+upload_base_dir=/model/fujii/hf_checkpoints/swallow-7b/imitative-open-asst2
 
 for ((i = start; i <= end; i += increment)); do
   upload_dir=$upload_base_dir/iter_$(printf "%07d" $i)
 
   python tools/model-upload/upload.py \
     --ckpt-path $upload_dir \
-    --repo-name tokyotech-llm/Swallow-13b-VE-instruct-ichikara-2epoch-iter$(printf "%07d" $i)
+    --repo-name tokyotech-llm/Swallow-7b-VE-instruct-imitative-open-asst2-2epoch-iter$(printf "%07d" $i)
 done
