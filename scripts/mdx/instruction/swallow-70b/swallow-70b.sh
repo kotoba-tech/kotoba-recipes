6cacdd7d85a87b9c188ac53ee67fc7cd5aea64ec
kazuki <kaz.tokyo.tech20@gmail.com>
2024-02-18 01:54:41 +0900

    feat: swallow 70b instruct

--- scripts/mdx/instruction/swallow-70b/swallow-70b.sh
+++ scripts/mdx/instruction/swallow-70b/swallow-70b.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
+#$ -l rt_AF=16
+#$ -l h_rt=0:9:30:00
+#$ -j y
+#$ -o outputs/instruction/swallow-70b/
+#$ -cwd
+
+# module load
+source /etc/profile.d/modules.sh
+module load cuda/11.8/11.8.0
+module load cudnn/8.9/8.9.2
+module load nccl/2.16/2.16.2-1
+module load hpcx/2.12
 
 # swich virtual env
 source .env/bin/activate
 
 # distributed settings
-JOB_ID=$(date +%s%N)
-export MASTER_ADDR=10.130.184.10
-export MASTER_PORT=12806
+export MASTER_ADDR=$(/usr/sbin/ip a show dev bond0 | grep 'inet ' | awk '{ print $2 }' | cut -d "/" -f 1)
+export MASTER_PORT=$((10000 + ($JOB_ID % 50000)))
 
 echo "MASTER_ADDR=${MASTER_ADDR}"
 
 # hostfile
-export NUM_GPU_PER_NODE=8
-NODE_TYPE="a100"
 
-NUM_NODES=16
+if [[ "$SGE_RESOURCE_TYPE" == "rt_F" ]]; then
+  export NUM_GPU_PER_NODE=4
+  NODE_TYPE="v100"
+elif [[ "$SGE_RESOURCE_TYPE" == "rt_AF" ]]; then
+  export NUM_GPU_PER_NODE=8
+  NODE_TYPE="a100"
+else
+  echo "Unrecognized SGE_RESOURCE_TYPE: $SGE_RESOURCE_TYPE"
+fi
+
+NUM_NODES=$NHOSTS
 NUM_GPUS=$((${NUM_NODES} * ${NUM_GPU_PER_NODE}))
 
 mkdir -p ./hostfile
-
-SGE_JOB_HOSTLIST=scripts/mdx/hostfile_16
-export SGE_JOB_HOSTLIST
 
 HOSTFILE_NAME=./hostfile/hostfile_${JOB_ID}
 while read -r line; do
@@ -41,21 +57,21 @@ WEIGHT_DECAY=0.1
 GRAD_CLIP=1
 
 # checkpoint & tokenizer
-TOKENIZER_MODEL=/model/fujii/hf_checkpoints/Swallow-70b-hf/tokenizer.model
-CHECKPOINT_DIR=/model/fujii/hf_checkpoints/Swallow-70b-hf/
-CHECKPOINT_SAVE_DIR="/model/fujii/checkpoints/Swallow-70b/lr_${LR}-minlr_${MIN_LR}"
+TOKENIZER_MODEL=/bb/llm/gaf51275/llama/huggingface-checkpoint/Swallow-70b-hf/tokenizer.model
+CHECKPOINT_DIR=/bb/llm/gaf51275/llama/huggingface-checkpoint/Swallow-70b-hf
+CHECKPOINT_SAVE_DIR="/bb/llm/gaf51275/llama/checkpoints/Swallow-70b-instruct/lr_${LR}-minlr_${MIN_LR}"
 
 mkdir -p ${CHECKPOINT_SAVE_DIR}
 
 # data config
 
-DATASET_DIR=/model/fujii/datasets/instruction/swallow/instruction/baseline
+DATASET_DIR=/bb/llm/gaf51275/llama/datasets/instruction/imitative-open-asst2
 
 TRAIN_DATA_PATH=${DATASET_DIR}/train.jsonl
 VALID_DATA_PATH=${DATASET_DIR}/val.jsonl
 
 # job name
-JOB_NAME="Swallow-70b-VE-BS=${GLOBAL_BATCH_SIZE}-LR=${LR}-MINLR=${MIN_LR}"
+JOB_NAME="Swallow-70b-VE-imitative-oasst2-BS=${GLOBAL_BATCH_SIZE}-LR=${LR}-MINLR=${MIN_LR}"
 
 # run
 mpirun -np $NUM_GPUS \
@@ -95,7 +111,6 @@ mpirun -np $NUM_GPUS \
   --save ${CHECKPOINT_SAVE_DIR} \
   --load ${CHECKPOINT_SAVE_DIR} \
   --low-cpu-fsdp \
-  --fsdp-cpu-offload \
   --sharding-strategy FULL_SHARD \
   --checkpoint-type LOCAL_STATE_DICT \
   --fsdp-activation-checkpointing \
